ee38218e0902ead6fe6687c090e7a4c0e049b982
wwmogu <57516881+wwmogu@users.noreply.github.com>
2023-08-18 23:56:32 +0930

    Update ubuntu22-04_first-boot.sh

--- ubuntu22-04_first-boot.sh
+++ ubuntu22-04_first-boot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # run it line by line
 sudo apt update && sudo apt upgrade
-sudo apt install build-essential cmake doxygen g++-9 gdb git git-lfs=2.9.2-1 python3-pip python3-venv libboost-dev libtinfo-dev libgtk-3-dev subversion tightvncserver \
+
+# git-lfs=2.9.2-1 was removed since it is not available.
+sudo apt install build-essential cmake doxygen g++-9 gdb git python3-pip python3-venv libboost-dev libtinfo-dev libgtk-3-dev subversion tightvncserver \
 valgrind unzip device-tree-compiler e2fsprogs gtkwave  software-properties-common python3.8 libpython2.7 \
 openssh-server net-tools libncursesw5 libsecret-1-0 appmenu-gtk2-module appmenu-gtk-module-common libcanberra-gtk-module libcanberra-gtk3-module -y --allow-downgrades
 # python
